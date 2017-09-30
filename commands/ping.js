8d81cf06728c8378bef8fe46899d94346375084e
BatchCoder101 <mickeyqquality@gmail.com>
2017-09-30 18:36:11 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -10,13 +10,14 @@ const talkedRecently = new Set();
                                 .setColor("#00FF00")
                                 .addField(`Ping: ${client.ping}`, `Woo! I'm feeling good! :slight_smile:`)
                                     message.channel.send(goodping);
-                            }
+                            } else {
                             if (`${client.ping}` <= 100 ) {
                                 let decentping = new Discord.RichEmbed()
                                 .setColor("#e6e600") 
                                 .addField(`Ping: ${client.ping}`, `It's not that bad, right? :confused:`)
                                 message.channel.send(decentping);
-                            } else {
+                            }
+                             } else {
                                 let  badping = new Discord.RichEmbed()
                                 .setColor("#8b0000")
                                 .addField(`Ping: ${client.ping}`, `Wow, this is higher than all my grades combined :slight_frown:`)
