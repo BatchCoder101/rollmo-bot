56a90981a5e2e1c7dd6f7ac149af6080d4f0c568
BatchCoder101 <mickeyqquality@gmail.com>
2017-10-01 04:00:10 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -5,19 +5,19 @@ const talkedRecently = new Set();
 
  exports.run = (client, message, args) => {
                     
-                            if (Math.round(`${client.ping}`) <= 50) { 
+                            if (Math.round`(${client.ping})` <= 50) { 
                             let  goodping = new Discord.RichEmbed()
                                 .setColor("#2ace2a")
                                 .addField(`Ping: ${client.ping}`, `Woo! I'm feeling good! :slight_smile:`)
                                     message.channel.send(goodping);
                             }
-                            if (Math.round(`${client.ping}`) > 51 && `${client.ping}` < 100) {
+                            if (Math.round`(${client.ping})` > 51 && `${client.ping}` < 100) {
                                 let decentping = new Discord.RichEmbed()
                                 .setColor("#e6e600") 
                                 .addField(`Ping: ${client.ping}`, `It's not that bad, right? :confused:`)
                                 message.channel.send(decentping);
                             } else {
-                             if (Math.round(`${client.ping}`) > 101) { 
+                             if (Math.round`(${client.ping})` > 101) { 
                                 let  badping = new Discord.RichEmbed()
                                 .setColor("#8b0000")
                                 .addField(`Ping: ${client.ping}`, `Wow, this is higher than all my grades combined :slight_frown:`)
