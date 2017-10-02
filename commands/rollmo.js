0ea1d769eb2965678ecefec8839799bd8e214cce
BatchCoder101 <mickeyqquality@gmail.com>
2017-10-02 02:07:35 -0500

    Update rollmo.js

--- commands/rollmo.js
+++ commands/rollmo.js
@@ -3,19 +3,21 @@ const client = new Discord.Client();
 const fs = require("fs");
 const talkedRecently = new Set();
 
-                    
+                   
 
 
 
 
        
  exports.run = (client, message, args) => {
+     let all = args[0]; 
+     let nocommand = args[0];
      
 
 // Let's pretend you mentioned the user you want to add a role to (!addrole @user Role Name):
 
 
-     
+    if (nocommand === undefined) { 
 const rolled = Math.floor((Math.random() * 6666) + 1);
 const premiumrolled = Math.floor(Math.random() * 3333) + 6667;
      
@@ -42,10 +44,33 @@ let toRoll = message.member
             if(message.member.roles.find("name", "Rollmo Premium") || message.member.hasPermission("ADMINISTRATOR")) { 
             message.channel.send(x2);
             toRoll.removeRole(prole).catch(console.error);
-        }
-    }     
- }
+        } 
+} 
 
+    }   else
+     if (all === 'all') {
+                                                  let noperms = new Discord.RichEmbed()
+                    .setColor("#8b0000")    
+                    .addField('Unable To Rollmo All', `You don't have permission!`);
+
+if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(noperms);
+     
+const rollall = Math.floor((Math.random() * 9999) + 1);
+     
+     var withBreaks = "- <@325012890656702465> \n- <@278277143522181121> \n- <@125319130579468288>"
+     
+            let  x = new Discord.RichEmbed()
+                                .setColor("#ff69b4")
+                                .addField(`Rollmo All`, `${message.author} rolled $${rollall} for everyone that is online!`)
+                                .addField(`Who can add your money?`, `- <@325012890656702465>`); //withBreaks
+     
+     if (message.member.hasPermission("ADMINISTRATOR")) {
+          message.guild.channels.find("name", "general").sendMessage(x);
+         message.delete();     
+ }
+         
+     }
+ }
   
              
      
