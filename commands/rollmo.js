ae0717e6fee888dd64e14fee84a8b798a33f3bc5
BatchCoder101 <mickeyqquality@gmail.com>
2017-10-05 16:20:15 -0500

    Update rollmo.js

--- commands/rollmo.js
+++ commands/rollmo.js
@@ -14,7 +14,7 @@ const talkedRecently = new Set();
      let nocommand = args[0];
      let help = args[0];
      let feedback = args[0];
-     let fbmessage = args[5];
+     let fbmessage = args[1];
 
 
     if (nocommand === undefined) { 
