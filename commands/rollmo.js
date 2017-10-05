7ee916da08447f190ca6b76d6ec8048b6547cf3d
BatchCoder101 <mickeyqquality@gmail.com>
2017-10-05 17:23:58 -0500

    Update rollmo.js

--- commands/rollmo.js
+++ commands/rollmo.js
@@ -81,7 +81,7 @@ const rollall = Math.floor(Math.random() * 4000) + 1001;
               if (feedback === 'feedback') {  
                     let x = new Discord.RichEmbed()
                          .setColor("#0e0d0d")    
-                         .addField(`${message.author.username}`, `${message.content}`);
+                         .addField(`${message.author.username}`, `${fbmessage}`);
      message.guild.channels.find("name", "feedback").send(x);
      message.delete();
      let y = new Discord.RichEmbed()
