9e83385f148c7f75c5d14d92a2b6a7677df57541
BatchCoder101 <mickeyqquality@gmail.com>
2017-10-05 10:25:57 -0500

    Update rollmo.js

--- commands/rollmo.js
+++ commands/rollmo.js
@@ -63,7 +63,7 @@ const rollall = Math.floor(Math.random() * 4000) + 1001;
             let  x = new Discord.RichEmbed()
                                 .setColor("#ff69b4")
                                 .addField(`Rollmo All`, `${message.author} rolled $${rollall} for everyone that is online!`)
-                                .addField(`Who can add your money?`, `- <@${message.author.id}>`); 
+                                .addField(`Who can add your money?`, `- ${message.author}`); 
      
      if (message.member.hasPermission("ADMINISTRATOR")) {
           message.guild.channels.find("name", "rollmo").send(x);
