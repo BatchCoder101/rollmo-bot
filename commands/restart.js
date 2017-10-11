cd675336efd1b282099475a61fa837dd540a1305
BatchCoder101 <mickeyqquality@gmail.com>
2017-10-11 03:21:01 -0500

    Add files via upload

---
+++ commands/restart.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const fs = require("fs");
+const talkedRecently = new Set();
+
+ exports.run = (client, message, args) => {
+                                                                                   let restarting = new Discord.RichEmbed()
+                         .setColor("#e6e600")    
+                         .addField(`Request To Restart`, `Restarting...`);
+     
+                                                                                        let restarted = new Discord.RichEmbed()
+                         .setColor("#2ace2a")    
+                         .addField(`Restart`, `I've been restarted!`);
+message.channel.send(restarting).then(() => {
+            client.destroy().then(() => {
+                process.exit(restarted)
+            })
+        })
+                        } 
+        
+             
+     
+     
+     
+module.exports.help = {
+    name: "restart"
+}
